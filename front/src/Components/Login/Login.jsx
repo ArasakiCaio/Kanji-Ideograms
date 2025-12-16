449905ad83cc379ce2abbc997fbf2dc24551b32f
Caio Arasaki <sasazaki08@gmail.com>
2025-12-16 03:19:20 -0300

    Estilização da tela de login

---
+++ front/src/Components/Login/Login.jsx
@@ -0,0 +1,49 @@
+import { FaUser, FaLock } from "react-icons/fa";
+
+import { useState } from "react";
+
+import "./Login.css";
+
+const Login = () => {
+
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    };
+    
+    return (
+        <div className="container">
+            <form onSubmit={handleSubmit}>
+                <h1>Acesse o sistema</h1>
+                <div className="input-field">
+                    <FaUser className="icon" />
+                    <input type="email" placeholder="Email"
+                    onChange={(e) => setUsername(e.target.value)}/>
+                </div>
+                <div className="input-field">
+                    <FaLock className="icon" />
+                    <input type="password" placeholder="Senha"
+                    onChange={(e) => setPassword(e.target.value)}/>
+                </div>
+
+                <div className="recall-forget">
+                    <label>
+                        <input type="checkbox"/>
+                        Lembrar de mim?
+                    </label>
+                    <a href="#">Esqueci minha Senha</a>
+                </div>
+
+                <button>Entrar</button>
+
+                <div className="signup-link">
+                    <a href="#">Quero me registrar</a>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default Login
